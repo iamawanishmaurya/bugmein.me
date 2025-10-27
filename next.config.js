61de2ade267995c536dd77d291658bfcdf12f19f
iamawanishmaurya <awanishmaurya9192@gmail.com>
2025-10-27 09:10:42 +0530

    Fix Next.js configuration for static export with Next.js 13.1.2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://bugmein.me' : '',
-  basePath: '',
-  distDir: 'out'
+  }
 }
 
 module.exports = nextConfig
